b135f8a07fb08b0d7841271c7f2df5da6d8e9349
jastar556 <jastar556@gmail.com>
2020-07-17 14:29:44 -0500

    AddedAccountTransactionDataSourceToRepo

---
+++ VirtualPiggyBank/DataSource/AccountTransactionTableDataSource.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Foundation;
+using UIKit;
+
+namespace VirtualPiggyBank.DataSource
+{
+    public class AccountTransactionTableDataSource : UITableViewSource
+    {
+        public AccountTransactionTableDataSource()
+        {
+        }
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override nint RowsInSection(UITableView tableview, nint section)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
