5355f729799a11f8df2923ece986ef2321aae5d4
jastar556 <jastar556@gmail.com>
2020-07-18 17:21:16 -0500

    Added SetingsViewController

---
+++ VirtualPiggyBank/SettingsViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace VirtualPiggyBank
+{
+    public partial class SettingsViewController : UIViewController
+    {
+        public SettingsViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
